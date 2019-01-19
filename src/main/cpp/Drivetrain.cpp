5421a2ce054d08a50b84a8e05ca9e2648c3cf12c
jeffding45 <jeffding45@outlook.com>
2019-01-18 19:12:11 -0600

    Talon current limits

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -179,6 +179,11 @@ void DriveTrain::Init(DriveMode mode)
 		m_lefttalon1->ConfigSelectedFeedbackSensor(ENC_LEFT_1, 0, 0);
 		m_lefttalon1->SetSensorPhase(false);
 		m_lefttalon1->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_lefttalon1->ConfigPeakCurrentLimit(30);
+		m_lefttalon1->ConfigPeakCurrentDuration(200);
+		m_lefttalon1->ConfigContinuousCurrentLimit(20);
+		m_lefttalon1->EnableCurrentLimit(true);
 	}
 
 	if (m_righttalon1 != nullptr)
@@ -186,30 +191,55 @@ void DriveTrain::Init(DriveMode mode)
 		m_righttalon1->ConfigSelectedFeedbackSensor(ENC_RIGHT_1, 0, 0);
 		m_righttalon1->SetSensorPhase(false);
 		m_righttalon1->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_righttalon1->ConfigPeakCurrentLimit(30);
+		m_righttalon1->ConfigPeakCurrentDuration(200);
+		m_righttalon1->ConfigContinuousCurrentLimit(20);
+		m_righttalon1->EnableCurrentLimit(true);
 	}
 
 	if (m_lefttalon2 != nullptr)
 	{
 		//m_lefttalon2->ConfigSelectedFeedbackSensor(ENC_LEFT_2, 0, 0);
 		//m_lefttalon2->SetSensorPhase(false);
-		//m_lefttalon2->SetNeutralMode(NeutralMode::Brake);
+		m_lefttalon2->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_lefttalon2->ConfigPeakCurrentLimit(30);
+		m_lefttalon2->ConfigPeakCurrentDuration(200);
+		m_lefttalon2->ConfigContinuousCurrentLimit(20);
+		m_lefttalon2->EnableCurrentLimit(true);
 	}
 
 	if (m_righttalon2 != nullptr)
 	{
 		//m_righttalon2->ConfigSelectedFeedbackSensor(ENC_RIGHT_2, 0, 0);
 		//m_righttalon2->SetSensorPhase(false);
-		//m_righttalon2->SetNeutralMode(NeutralMode::Brake);
-	}
+		m_righttalon2->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_righttalon2->ConfigPeakCurrentLimit(30);
+		m_righttalon2->ConfigPeakCurrentDuration(200);
+		m_righttalon2->ConfigContinuousCurrentLimit(20);
+		m_righttalon2->EnableCurrentLimit(true);
+}
 
 	if (m_lefttalon3 != nullptr)
 	{
 		m_lefttalon3->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_lefttalon3->ConfigPeakCurrentLimit(30);
+		m_lefttalon3->ConfigPeakCurrentDuration(200);
+		m_lefttalon3->ConfigContinuousCurrentLimit(20);
+		m_lefttalon3->EnableCurrentLimit(true);
 	}
 
 	if (m_righttalon3 != nullptr)
 	{
 		m_righttalon3->SetNeutralMode(NeutralMode::Brake);
+		// configure talon current peak=30A for 200ms, continuous=20A
+		m_righttalon3->ConfigPeakCurrentLimit(30);
+		m_righttalon3->ConfigPeakCurrentDuration(200);
+		m_righttalon3->ConfigContinuousCurrentLimit(20);
+		m_righttalon3->EnableCurrentLimit(true);
 	}
 
 	m_leftpow = 0;
