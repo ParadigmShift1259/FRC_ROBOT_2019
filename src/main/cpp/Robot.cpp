237337d29e291ea1609b86c2713320d77a03af44
GeoffreyX <xuegeo21@pewaukeeschools.org>
2019-06-25 14:52:38 -0500

    Removing Whitespace

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -19,31 +19,12 @@ bool Debug = true;
 
 void Robot::RobotInit()
 {
-
 	m_operatorinputs = new OperatorInputs();
 	m_drivetrain = new Drivetrain(m_operatorinputs);
 }
 
 
-void Robot::RobotPeriodic()
-{
-}
-
-
-/*
- * This autonomous (along with the chooser code above) shows how to
- * select
- * between different autonomous modes using the dashboard. The sendable
- * chooser code works with the Java SmartDashboard. If you prefer the
- * LabVIEW Dashboard, remove all of the chooser code and uncomment the
- * GetString line to get the auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to
- * the
- * if-else structure below with additional strings. If using the
- * SendableChooser make sure to add them to the chooser code above as
- * well.
- */
+void Robot::RobotPeriodic(){}
 void Robot::AutonomousInit(){}
 void Robot::AutonomousPeriodic(){}
 void Robot::TestInit(){}
